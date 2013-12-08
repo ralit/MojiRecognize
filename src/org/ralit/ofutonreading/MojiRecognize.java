3d3ff0f0954f85a5dc423415d80c61b3e167ee05
ralit <ralit@ralitMacBookProMid2012.local>
2013-12-08 11:50:16 +0900

    関数に分割

--- src/org/ralit/ofutonreading/MojiRecognize.java
+++ src/org/ralit/ofutonreading/MojiRecognize.java
@@ -2,6 +2,7 @@ package org.ralit.ofutonreading;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
@@ -19,7 +20,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 class MojiRecognize extends Thread {
-	
+
 	/**
 	 * 使用ライブラリ
 	 * httpclient-4.2.3
@@ -29,19 +30,19 @@ class MojiRecognize extends Thread {
 	 * jackson-core-2.2.0
 	 * jackson-databind-2.2.0
 	 */
-	
+
 	private String mFilePath;
-	
+
 	public MojiRecognize(String filePath) {
 		mFilePath = filePath;
 	}
-	
-	public void run() {
+
+	private HttpResponse requestJobID() {
 		try {
 			// HTTPClientはどっちかを使う
 			// 1. org.apache.http.impl.client.DefaultHttpClient
 			// 2. android.net.http.AndroidHttpClient
-//			DefaultHttpClient client = new DefaultHttpClient(); // (1) こっちも動きました
+			// DefaultHttpClient client = new DefaultHttpClient(); // (1) こっちも動きました
 			AndroidHttpClient client = AndroidHttpClient.newInstance("Android UserAgent"); // (2)
 			HttpPost post = new HttpPost("https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/scene?APIKEY=" + DocomoAPI.getApi());
 			// これを知らなかった。MultipartのPOSTをするときはこのクラスを使おう。
@@ -51,56 +52,93 @@ class MojiRecognize extends Thread {
 			post.setEntity(multipartEntity);
 			// 通信開始
 			HttpResponse response = client.execute(post);
+			return response;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+	
+	private String parseJobID(HttpResponse response) {
+		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
 			StringBuilder builder = new StringBuilder();
 			String line = null;
 			while ((line = reader.readLine()) != null) {
 				builder.append(line + "\n");
 			}
-			// JSONパース(jobIDの取得)
 			JsonNode jsonNode = new ObjectMapper().readTree(builder.toString());
 			String jobID = jsonNode.path("job").path("@id").asText();
+			return jobID;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private JsonNode requestResult(String jobID) {
+		try { 
+			AndroidHttpClient client = AndroidHttpClient.newInstance("Android UserAgent");
+			HttpGet get = new HttpGet("https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/scene/" + jobID + "?APIKEY=" + DocomoAPI.getApi());
+			get.setHeader("Content-Type", "application/json");
+			HttpResponse response = client.execute(get);
+			// HttpResponseのEntityデータをStringへ変換
+			BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+			StringBuilder builder = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				builder.append(line + "\n");
+			}
+			JsonNode jsonNode = new ObjectMapper().readTree(builder.toString());
+			return jsonNode;
+		} catch(Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	private ArrayList<Word> parseMoji(JsonNode jsonNode) {
+		JsonNode wordNode = null;
+		ArrayList<Word> wordList = new ArrayList<Word>();
+		log(jsonNode.toString());
+		for (int i = 0; (wordNode = jsonNode.path("words").path("word").get(i)) != null; i++) {
+			Word word = new Word();
+			JsonNode pointNode = wordNode.path("shape").path("point");
+//			log(pointNode.toString());
+			word.setPoint(pointNode.get(0).path("@x").asInt(), pointNode.get(0).path("@y").asInt(), pointNode.get(2).path("@x").asInt(), pointNode.get(2).path("@y").asInt());
+//			log(String.valueOf(word.getArea()));
+			word.setText(wordNode.path("@text").asText());
+			word.setScore(wordNode.path("@score").asInt());
+			wordList.add(word);
+		}
+		return wordList;
+	}
+	
+	public void run() {
+		try {
+			// 画像を送ってjobIDをもらう
+			HttpResponse response = requestJobID();
+			// JSONパース(jobIDの取得)
+			String jobID = parseJobID(response);
 			// 認識結果取得待ち
 			int waitingTime = 0;
+			JsonNode jsonNode = null;
 			while(true) {
 				Thread.sleep(1000);
-				HttpGet get = new HttpGet("https://api.apigw.smt.docomo.ne.jp/characterRecognition/v1/scene/" + jobID + "?APIKEY=" + DocomoAPI.getApi());
-				get.setHeader("Content-Type", "application/json");
-				response = client.execute(get);
-				// HttpResponseのEntityデータをStringへ変換
-				reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
-				builder = new StringBuilder();
-				line = null;
-				while ((line = reader.readLine()) != null) {
-					builder.append(line + "\n");
-				}
-				jsonNode = new ObjectMapper().readTree(builder.toString());
+				// 認識結果をリクエスト
+				jsonNode = requestResult(jobID);
 				String status = jsonNode.path("job").path("@status").asText();
 				if (!status.equals("process") && !status.equals("queue")) { break; }
-				waitingTime++;
-				log("status: " + status + " (" + waitingTime + " sec)");
+				log("status: " + status + " (" + ++waitingTime + " sec)");
 			}
-			// JSONパース(
-			JsonNode wordNode = null;
-			ArrayList<Word> wordList = new ArrayList<Word>();
-			log(jsonNode.toString());
-			for (int i = 0; (wordNode = jsonNode.path("words").path("word").get(i)) != null; i++) {
-				Word word = new Word();
-				JsonNode pointNode = wordNode.path("shape").path("point");
-//				log(pointNode.toString());
-				word.setPoint(pointNode.get(0).path("@x").asInt(), pointNode.get(0).path("@y").asInt(), pointNode.get(2).path("@x").asInt(), pointNode.get(2).path("@y").asInt());
-//				log(String.valueOf(word.getArea()));
-				word.setText(wordNode.path("@text").asText());
-				word.setScore(wordNode.path("@score").asInt());
-				wordList.add(word);
-			}
-
-			
-
+			// 認識結果が返ってきた
+			// JSONパース(文字情報)
+			ArrayList<Word> wordList = parseMoji(jsonNode);
+			log(String.valueOf(wordList.size()));
 		} catch (Exception e) {
 			e.getStackTrace();
 		}
-
 	}
 
 
